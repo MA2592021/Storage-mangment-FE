3d76186b13a17febfa135b06e753cfb8274b9339
amir said <109632495+MA2592021@users.noreply.github.com>
2023-04-20 11:05:18 +0200

    employee page 90%

--- src/components/layout/tabs.vue
+++ src/components/layout/tabs.vue
@@ -4,7 +4,6 @@
       <v-tab :value="1" router :to="`${parentPath}dashboard`">Dashboard</v-tab>
       <v-tab :value="2" router :to="`${parentPath}all`">All</v-tab>
       <v-tab :value="3" router :to="`${parentPath}add`">Add</v-tab>
-      <v-tab :value="4" disabled>details</v-tab>
     </v-tabs>
     <v-window v-model="tab">
       <v-window-item v-for="n in 3" :key="n" :value="n">
@@ -27,6 +26,7 @@ export default {
     parentPath() {
       return this.$route.matched[0].path;
     },
+
   },
 };
 </script>
--- src/views/employee/employeePage.vue
+++ src/views/employee/employeePage.vue
@@ -1,13 +1,194 @@
 <template>
-  <div>Employee page</div>
+  <v-card class="mx-auto" elevation="2" style="width: 100%"
+    ><v-row
+      ><v-col cols="3" sm="2"
+        ><v-img
+          class="bg-white"
+          width="300"
+          :aspect-ratio="1"
+          src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+          cover
+        ></v-img></v-col
+      ><v-col cols="9" sm="10">
+        <v-row>
+          <v-col cols="12" sm="6" md="6">
+            <v-text-field
+              label="Name "
+              required
+              :readonly="dis === true"
+              v-model="name"
+              variant="underlined"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" sm="6" md="6">
+            <v-text-field
+              required
+              label="Code "
+              :readonly="dis === true"
+              v-model="code"
+              variant="underlined"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" sm="6" md="6">
+            <v-text-field
+              label="Phone number "
+              variant="underlined"
+              :readonly="dis === true"
+              v-model="phone"
+              required
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" sm="6" md="6">
+            <v-text-field
+              required
+              v-model="nid"
+              label="National ID "
+              :readonly="dis === true"
+              variant="underlined"
+            ></v-text-field>
+          </v-col>
+
+          <v-col cols="12" sm="6">
+            <v-autocomplete
+              label="Role "
+              v-model="role"
+              variant="underlined"
+              :readonly="dis === true"
+              :items="['Supervisor', 'Employee']"
+            ></v-autocomplete>
+          </v-col>
+          <v-col cols="12" sm="6"
+            ><v-textarea
+              clearable
+              label="Note"
+              v-model="note"
+              :readonly="dis === true"
+              prepend-icon="mdi-note-text-outline"
+            ></v-textarea>
+          </v-col>
+        </v-row> </v-col></v-row
+    ><v-card-actions class="mx-auto">
+      <v-btn @click="dis = !dis"> Edit </v-btn>
+      <v-btn class="ml-auto" :disabled="dis === true"> Save </v-btn>
+    </v-card-actions></v-card
+  >
+  <v-card class="mt-3" style="width: 100%">
+    <v-expansion-panels variant="popout" class="my-4">
+      <v-expansion-panel>
+        <v-expansion-panel-title>
+          <template v-slot:default="{ expanded }">
+            <v-row no-gutters>
+              <v-col cols="4" class="d-flex justify-start"> properties </v-col>
+              <v-col cols="8" class="text-grey">
+                <v-fade-transition leave-absolute>
+                  <span v-if="expanded" key="0">
+                    properties with {{ name }}
+                  </span>
+                  <span v-else key="1">
+                    {{ name }}
+                  </span>
+                </v-fade-transition>
+              </v-col>
+            </v-row>
+          </template>
+        </v-expansion-panel-title>
+        <v-expansion-panel-text>
+          <tt v-bind:data="data" v-bind:header="headers" />
+        </v-expansion-panel-text>
+      </v-expansion-panel>
+
+      <v-expansion-panel>
+        <v-expansion-panel-title>
+          <template v-slot:default="{ expanded }">
+            <v-row no-gutters>
+              <v-col cols="4" class="d-flex justify-start"> materials </v-col>
+              <v-col cols="8" class="text-grey">
+                <v-fade-transition leave-absolute>
+                  <span v-if="expanded" key="0">
+                    materials with {{ name }}
+                  </span>
+                  <span v-else key="1">
+                    {{ name }}
+                  </span>
+                </v-fade-transition>
+              </v-col>
+            </v-row>
+          </template>
+        </v-expansion-panel-title>
+        <v-expansion-panel-text>
+          <tt v-bind:data="data1" v-bind:header="headers1" />
+        </v-expansion-panel-text>
+      </v-expansion-panel> </v-expansion-panels
+  ></v-card>
 </template>
 
 <script>
+import tt from "../../components/table.vue";
 export default {
-  setup() {
-    return {};
-  },
+  components: { tt },
+  data: () => ({
+    name: "el gamal",
+    code: "2232",
+    role: "supervisor",
+    nid: "12141243523123",
+    phone: "01110133639",
+    img: "",
+    dis: true,
+    note: "lailo lailo lailo la la la ",
+    headers: [
+      {
+        align: "start",
+        key: "ID",
+        sortable: false,
+        title: "ID",
+      },
+      { title: "property", key: "property" },
+      { title: "quantity", key: "quantity" },
+    ],
+    data: [
+      {
+        ID: "1",
+        property: "ma2s",
+        quantity: "14",
+      },
+      {
+        ID: "2",
+        property: "ma2sat",
+        quantity: "4",
+      },
+      {
+        ID: "12",
+        property: "mastra",
+        quantity: "2",
+      },
+    ],
+    headers1: [
+      {
+        align: "start",
+        key: "ID",
+        sortable: false,
+        title: "ID",
+      },
+      { title: "material", key: "material" },
+      { title: "quantity", key: "quantity" },
+    ],
+    data1: [
+      {
+        ID: "1",
+        material: "2omash white",
+        quantity: "14",
+      },
+      {
+        ID: "2",
+        material: "habwww",
+        quantity: "4",
+      },
+      {
+        ID: "12",
+        material: "2omash ezwd",
+        quantity: "2",
+      },
+    ],
+  }),
 };
 </script>
-
-<style lang="scss" scoped></style>
