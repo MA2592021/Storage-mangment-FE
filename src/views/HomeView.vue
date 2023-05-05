ac99b269d7aa7500bcf4483127ddc8159cbd93a3
ma2592021 <109632495+ma2592021@users.noreply.github.com>
2023-05-05 16:40:12 +0200

    testss

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,6 +9,14 @@
     <v-card-item>
       <div>
         <div class="text-overline mb-1">stage number {{ index + 1 }}</div>
+        <v-autocomplete
+          v-model="selected"
+          :items="data"
+          item-title="name"
+          label="select stage"
+          return-object
+          @update:modelValue="dosomthing(selected, stage.id)"
+        ></v-autocomplete>
         <div class="text-overline mb-1">{{ stage.name }}</div>
         <div class="text-caption">{{ stage.desc }}</div>
         <div class="text-caption">{{ stage.id }}</div>
@@ -17,6 +25,8 @@
 
     <v-card-actions>
       <v-btn variant="outlined" @click="deletee(stage.id)"> Delete </v-btn>
+      <v-btn variant="outlined" @click="up(stage.id)"> up </v-btn>
+      <v-btn variant="outlined" @click="down(stage.id)"> down </v-btn>
     </v-card-actions>
   </v-card>
   <v-btn class="mt-4" @click="addstage()">Add Stage</v-btn>
@@ -35,6 +45,12 @@ export default {
       stages: [{ id: "0", name: "stage name", desc: "stage discribtion" }],
 
       id: 0,
+      selected: null,
+      data: [
+        { name: "2as", id: "1", desc: "bn2os" },
+        { name: "geb", id: "2", desc: "bngeb" },
+        { name: "manika", id: "3", desc: "bnmanik" },
+      ],
     };
   },
   methods: {
@@ -52,6 +68,40 @@ export default {
         this.stages.findIndex((m) => m.id === id),
         1
       );
+    },
+    up(id) {
+      console.log("im alive");
+
+      const index = this.stages.findIndex((m) => m.id === id);
+      if (index - 1 === -1) {
+      } else {
+        const element = this.stages.find((m) => m.id === id);
+        this.deletee(id);
+        this.stages.splice(index - 1, 0, element);
+        console.log(element, index);
+      }
+    },
+    down(id) {
+      console.log("im alive");
+
+      const index = this.stages.findIndex((m) => m.id === id);
+      if (index + 1 === this.stages.length) {
+      } else {
+        console.log(index, this.stages.length);
+        const element = this.stages.find((m) => m.id === id);
+        this.deletee(id);
+        this.stages.splice(index + 1, 0, element);
+        // console.log(element, index);
+      }
+    },
+    dosomthing(selected, index) {
+      console.log("im alive");
+      console.log(selected);
+      console.log(this.stages.find((m) => m.id === index));
+      this.stages.find((m) => m.id === index).name = selected.name;
+      this.stages.find((m) => m.id === index).desc = selected.desc;
+      this.stages.find((m) => m.id === index).id = selected.id;
+      this.selected = null;
     },
   },
 
